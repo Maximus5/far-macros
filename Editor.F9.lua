29f07e6569945e155f1b6d5fb700fe37567de8f3
Maximus5 <ConEmu.Maximus5@gmail.com>
2016-02-04 20:03:03 +0300

    Editor: Save and reload lua macros

---
+++ Editor.F9.lua
@@ -0,0 +1,15 @@
+﻿
+Macro {
+  area="Editor";
+  key="F2 F9";
+  description="Save and reload lua macros";
+condition = function() return mf.ucase(mf.fsplit(Editor.FileName, 8)) == ".LUA" end;
+action = function()
+  -- Save file, if it was modified
+  if band(Editor.State,8) then
+    Keys("F2")
+  end
+  --
+  far.MacroLoadAll()
+end;
+}
